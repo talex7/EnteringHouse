15ea2bb2a3ebc7080021137be977e1d64796f70b
talex7 <talexanian@gmail.com>
2016-11-13 15:24:12 -0500

    Final Commit

--- EnteringHouse/ViewController.m
+++ EnteringHouse/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *navigationBar;
+@property (strong, nonatomic) IBOutlet UIView *frontDoorView;
+@property (weak, nonatomic) UIBarButtonItem * rightBarButtonItem;
 
 @end
 
@@ -17,7 +19,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    }
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Front Door" style:UIBarButtonItemStylePlain target:self action:@selector(goToStart)];
+    
+}
 
 
 - (void)didReceiveMemoryWarning {
@@ -25,5 +29,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+-(void)goToStart {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+     
 @end
